db06a4130313751abac470148528067d0d5c91fb
Maya Gao <mayagao@github.com>
2025-03-17 22:10:38 -0700

    remove

--- src/app/components/APIContent.tsx
+++ src/app/components/APIContent.tsx
@@ -7,17 +7,5 @@ export function APIContent() {
 
   if (!currentPrimitive) return null;
 
-  return (
-    <div>
-      <p>Type: {currentPrimitive.type}</p>
-      <p>Title: {currentPrimitive.title}</p>
-      <a
-        target="_blank"
-        className="link text-blue-600 underline"
-        href={currentPrimitive.url}
-      >
-        url: {currentPrimitive.url}
-      </a>
-    </div>
-  );
+  return <div></div>;
 }
